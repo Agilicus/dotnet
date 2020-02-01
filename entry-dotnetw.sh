0eb534b4fcf369605ce8d612fe924bb7d68f793b
Don Bowman <don@agilicus.com>
2020-02-01 16:27:08 -0500

    fix: remove run-time ngen

--- entry-dotnetw.sh
+++ entry-dotnetw.sh
@@ -19,8 +19,8 @@ log "Startup"
 rm -f /tmp/.X1-lock
 log "Start Xvfb"
 Xvfb :1 &
-log "Update .NET bits (foreground) ..."
-wine /home/dotnet/.wine/drive_c/windows/Microsoft.NET/Framework/v4.0.30319/ngen.exe update /nologo /force ||true
+#log "Update .NET bits (foreground) ..."
+#wine /home/dotnet/.wine/drive_c/windows/Microsoft.NET/Framework/v4.0.30319/ngen.exe update /nologo /force ||true
 log "Start fastcgi"
 wine /home/dotnet/.wine/drive_c/windows/Microsoft.NET/assembly/GAC_32/fastcgi-mono-server4/v4.0_0.4.0.0__0738eb9f132ed756/fastcgi-mono-server4.exe --applications=/:. --socket=tcp:127.0.0.1:9000 &
 wait_ready
